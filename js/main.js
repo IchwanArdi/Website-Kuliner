3a912d5a2675af7a8c41516d625aa3ede0c4fb21
IchwanArdi <ichwanpwt22@gmail.com>
2025-01-15 00:10:04 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -8,6 +8,19 @@ toggle.addEventListener('click', () => {
   });
 });
 
+// script untuk slide makanan
+const foodGallery = document.getElementById('foodGallery');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+prevBtn.addEventListener('click', () => {
+  foodGallery.scrollBy({ left: -300, behavior: 'smooth' });
+});
+
+nextBtn.addEventListener('click', () => {
+  foodGallery.scrollBy({ left: 300, behavior: 'smooth' });
+});
+
 // Json untuk tampilan restoran
 $.getJSON('data/restoran.json', function (data) {
   let restoran = data.restoran;
@@ -15,7 +28,7 @@ $.getJSON('data/restoran.json', function (data) {
   $.each(restoran, function (i, data) {
     $('#daftar-restoran').append(`<div class="col-md d-flex justify-content-center">
             <div class="card mt-3 opacity-90" style="width: 18rem">
-              <img src="img/restoran/${data.gambar}" class="card-img-top" alt="restoran1" />
+              <img src="img/restoran/${data.gambar}" class="card-img-top gambar-makanan" alt="restoran1" />
               <div class="card-body">
                 <h5 class="card-title">${data.nama}</h5>
                 
@@ -25,9 +38,23 @@ $.getJSON('data/restoran.json', function (data) {
                 <li class="list-group-item"><em>${data.deskripsi}</em></li>
               </ul>
               <div class="card-body">
-                <button class="btn btn-success m-0" type="button"> Detail </button>
+                <button class="btn btn-success " type="button"> Detail </button>
               </div>
             </div>
+          </div>`);
+  });
+});
+
+// Json untuk tampilan makanan
+$.getJSON('data/restoran.json', function (data) {
+  let makanan = data.restoran;
+  console.log(makanan);
+
+  $.each(makanan, function (i, data) {
+    $('#foodGallery').append(`<div class="food-item me-3">
+            <img src="img/restoran/${data.gambar}" class="gambar-makanan rounded" style="width: 350px; height: 250px; object-fit: cover" alt="Mendoan" />
+            <p class=" mt-2 fw-bold text-white">Mendoan</p>
+            
           </div>`);
   });
 });
