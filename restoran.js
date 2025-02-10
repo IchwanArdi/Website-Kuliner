f89989a60916ac0590da863554a31c54c7a7481a
IchwanArdi <ichwanpwt22@gmail.com>
2025-02-10 11:15:46 +0700

    first commit

---
+++ restoran.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function loadRestoran() {
+  try {
+    const fileData = './data/restoran.json';
+    const dataBuffer = fs.readFileSync(fileData, 'utf-8'); // Membaca file
+    const data = JSON.parse(dataBuffer); // Parsing data JSON
+    return data.restoran; // Mengembalikan array `restoran`
+  } catch (err) {
+    console.error('Error membaca file restoran.json:', err);
+    return []; // Kembalikan array kosong jika terjadi error
+  }
+}
+
+// detail contact berdasarkan nama
+const detailRestoran = (nama) => {
+  const restoran = loadRestoran();
+  const detail = restoran.find((detail) => detail.nama === nama);
+  return detail;
+};
+
+function loadMakanan() {
+  try {
+    const fileData = './data/makanan.json';
+    const dataBuffer = fs.readFileSync(fileData, 'utf-8'); // Membaca file
+    const data = JSON.parse(dataBuffer); // Parsing data JSON
+    return data.makanan; // Mengembalikan array `restoran`
+  } catch (err) {
+    console.error('Error membaca file restoran.json:', err);
+    return []; // Kembalikan array kosong jika terjadi error
+  }
+}
+
+module.exports = { loadRestoran, detailRestoran, loadMakanan };
